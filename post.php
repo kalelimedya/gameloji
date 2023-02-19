b503416b2197c3f1320a754eb0ae6b1a46ffe974
YigithanGumus <yigithangumuss@gmail.com>
2023-02-19 04:23:18 +0300

    a
    a

--- post.php
+++ post.php
@@ -21,7 +21,7 @@
 
  <?php while($sorgucek=$sorgu->fetch(PDO::FETCH_ASSOC)) { ?>
 
-       <section class="welcome_section layout_padding">
+  <section class="welcome_section layout_padding">
     <div class="container">
       <div class="custom_heading-container">
         <h2>
@@ -33,9 +33,26 @@
           <img src="images/welcome.png" alt=""/>
         </div>
         <div class="detail-box">
-          <p>
-              <?php echo $sorgucek["blog_text"] ?>
-          </p>
+          <div class="container">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
           <div>
             <a href="">
               Read More
