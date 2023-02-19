6efead10a8c9eb719d97b7817077189a05fa6d80
erenekmekci <erenekm@gmail.com>
2023-02-19 04:01:15 +0300

    asd

--- post.php
+++ post.php
@@ -3,6 +3,21 @@
     $sorgu->execute();
  ?>
 
+<style>
+
+.blog-text {
+	
+    color: var(--light-gray);
+    font-size: var(--fs-6);
+    font-weight: var(--fw-300);
+    line-height: 1.6;
+	text-align: center;
+}
+
+
+</style>
+
+
  <div class="container">
  <?php while($sorgucek=$sorgu->fetch(PDO::FETCH_ASSOC)) { ?>
 
@@ -14,10 +29,17 @@
  <div class="service_container layout_padding2">
 
 
-
-
-
- 
+          <div style="  height: 403px;" class="service_box">
+          <div style="text-align: center;" class="img-box">
+            <img style="height: 250px;" src="images/<?php echo $sorgucek['blog_img'] ?>" alt="<?php echo $sorgucek['blog_header'] ?>"/>
+          </div>
+		  </div>
+<div class="blog-text">
+<p>
+             
+              <?php echo $sorgucek["blog_text"] ?>
+            </p>
+ </div>
 </div>
 </div>
   <?php } ?>
