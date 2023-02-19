aedbee3b598e1870be79d257151757853f12dc09
erenekmekci <erenekm@gmail.com>
2023-02-19 03:01:57 +0300

    Create post.php

---
+++ post.php
@@ -0,0 +1,27 @@
+<?php include 'header.php';
+    $sorgu=$db->prepare("SELECT * FROM blog");
+    $sorgu->execute();
+ ?>
+
+ <div class="container">
+ <?php while($sorgucek=$sorgu->fetch(PDO::FETCH_ASSOC)) { ?>
+
+       <div class="custom_heading-container custom_heading-container">
+        <h2 class="custom_heading-container">
+         <?php echo $sorgucek["blog_header"] ?>
+        </h2>
+      </div>
+ <div class="service_container layout_padding2">
+
+
+
+
+
+ 
+</div>
+</div>
+  <?php } ?>
+
+
+<?php include 'footer.php'; ?>
+
